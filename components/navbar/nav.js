c80503c4e940d55d786766cba17f96659e75755b
Nada-AlaaEldeen <104720889+NadaAlaaEldeen@users.noreply.github.com>
2023-03-03 07:51:15 +0200

    Update nav.js

--- components/navbar/nav.js
+++ components/navbar/nav.js
@@ -8,14 +8,14 @@ let navbar=
     <li><a href="product.html">Product</a></li>
     <li><a href="about.html">About</a></li>
     <li><a href="contact.html">Contact</a></li>
-    <li >
-    <a style="padding-top: 8px" href="cart.html">
+    <li style="padding-top: 8px" >
+    <a  href="cart.html">
     <i class="bi bi-cart-check-fill fs-3 icon-black"> 
     </i>
     <sup style="font-size:1.5vw;color:red">0</sup>
     </a>
     </li>
-    <li >
+    <li style="padding-top: 8px" >
     <a href="login.html"> <i class="bi bi-person fs-3"></i></a>
     </li>
       <a href="#" id="close" ><i class="bi bi-x fs-1"></i></a>
