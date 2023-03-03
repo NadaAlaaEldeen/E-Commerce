00c2ef93071862ab1b8d0e8e63c674f81970c089
Yousef Adel <yousefjooo2018@gmail.com>
2023-03-03 15:35:30 +0200

    edit the photos path

--- script/products_arr.js
+++ script/products_arr.js
@@ -1,20 +1,20 @@
 let products = [
     {
       product_name: "t-shirt",
-      product_img: "/images/products2/f1.jpg",
-      product_img1: "/images/products2/f2.jpg",
-      product_img2: "/images/products2/f3.jpg",
-      product_img3: "/images/products2/f4.jpg",
+      product_img: "images/products2/f1.jpg",
+      product_img1: "images/products2/f2.jpg",
+      product_img2: "images/products2/f3.jpg",
+      product_img3: "images/products2/f4.jpg",
       description:"T-shirt in soft cotton jersey. Regular Fit.",
       category: "topwear",
       price: "150 EGP",
     },
     {
       product_name: "shirt",
-      product_img: "/images/products2/n1.jpg",
-      product_img1: "/images/products2/n2.jpg",
-      product_img2: "/images/products2/n3.jpg",
-      product_img3: "/images/products2/n5.jpg",
+      product_img: "images/products2/n1.jpg",
+      product_img1: "images/products2/n2.jpg",
+      product_img2: "images/products2/n3.jpg",
+      product_img3: "images/products2/n5.jpg",
       description:"shirt in soft cotton jersey. Regular Fit.",
    
       category: "topwear",
@@ -22,10 +22,10 @@ let products = [
     },
     {
       product_name: "Regular Fit Long Sleeve Oxford Men's Shirt",
-      product_img: "/images/p/p13.jpg",
-      product_img1: "/images/p/p14.jpg",
-      product_img2: "/images/p/p11.jpg",
-      product_img3: "/images/p/p12.jpg",
+      product_img: "images/p/p13.jpg",
+      product_img1: "images/p/p14.jpg",
+      product_img2: "images/p/p11.jpg",
+      product_img3: "images/p/p12.jpg",
       description:`Button collar
       Oxford fabric
       One chest pocket`,
@@ -35,9 +35,9 @@ let products = [
     {
       product_name: "Slim Fit Biker Collar Men's Coat",
       product_img: "images/products2/LC_j.jpg",
-      product_img1: "/images/p/p21.jpg",
-      product_img2: "/images/p/p22.jpg",
-      product_img3: "/images/p/p23.jpg",
+      product_img1: "images/p/p21.jpg",
+      product_img2: "images/p/p22.jpg",
+      product_img3: "images/p/p23.jpg",
       description:`With a stylish and modern style, the men's coat fits the body with its slim fit cut. The biker collar and the coat, which stands out with its energy, combines classic and sporty elegance.`,
       category: "jacket",
       price: "1000 EGP",
@@ -49,13 +49,23 @@ let products = [
       category: "bottomwear",
       price: "150 EGP",
     },
+    {
+      product_name: "Slim Fit Men's Jogger Sweatpants",
+      product_img: "images/p/p41.jpg",
+      product_img1: "images/p/p42.jpg",
+      description:`Zipper pocket
+      Waist with elastic and adjustable drawstring`,
+
+      category: "bottomwear",
+      price: "300 EGP",
+    },
 
     {
         product_name: "Shirt Collar Straight Long Sleeve Women's Tunic",
-        product_img: "/images/p/p61.jpg",
-        product_img1: "/images/p/p62.jpg",
-        product_img2: "/images/p/p63.jpg",
-        product_img3: "/images/p/p64.jpg",
+        product_img: "images/p/p61.jpg",
+        product_img1: "images/p/p62.jpg",
+        product_img2: "images/p/p63.jpg",
+        product_img3: "images/p/p64.jpg",
         description:`wonderful flower pant`,
         category: "topwear",
         price: "650 EGP",
@@ -91,10 +101,10 @@ let products = [
 
     {
         product_name: "Crew Neck Regular Long Sleeve Women's Tricot Sweater",
-        product_img: "/images/p/p71.jpg",
-        product_img1: "/images/p/p72.jpg",
-        product_img2: "/images/p/p73.jpg",
-        product_img3: "/images/p/p74.jpg",
+        product_img: "images/p/p71.jpg",
+        product_img1: "images/p/p72.jpg",
+        product_img2: "images/p/p73.jpg",
+        product_img3: "images/p/p74.jpg",
 
         description:`Ribbed ankles and bottom`,
         category: "topwear",
@@ -103,10 +113,10 @@ let products = [
 
     {
         product_name: "Brown Jacket Collar Regular Long Sleeve Women's Cachet Coat",
-        product_img: "/images/p/p53.jpg",
-        product_img1: "/images/p/p54.jpg",
-        product_img2: "/images/p/p51.jpg",
-        product_img3: "/images/p/p52.jpg",
+        product_img: "images/p/p53.jpg",
+        product_img1: "images/p/p54.jpg",
+        product_img2: "images/p/p51.jpg",
+        product_img3: "images/p/p52.jpg",
         description:`Waistband
         Front double pocket
         made of stamp fabric`,
@@ -115,10 +125,10 @@ let products = [
     },
     {
       product_name: "Crew Neck Short Sleeve Combed Cotton Men's T-shirt",
-      product_img: "/images/p/p31.jpg",
-      product_img1: "/images/p/p32.jpg",
-      product_img2: "/images/p/p33.jpg",
-      product_img3: "/images/p/p34.jpg",
+      product_img: "images/p/p31.jpg",
+      product_img1: "images/p/p32.jpg",
+      product_img2: "images/p/p33.jpg",
+      product_img3: "images/p/p34.jpg",
       description:`From combed cotton fabric short sleeved T-Shirt`,
       category: "topwear",
       price: "200 EGP",
@@ -130,16 +140,7 @@ let products = [
       category: "shoes",
       price: "600 EGP",
     },
-    {
-        product_name: "Slim Fit Men's Jogger Sweatpants",
-        product_img: "/images/p/p41.jpg",
-        product_img1: "/images/p/p42.jpg",
-        description:`Zipper pocket
-        Waist with elastic and adjustable drawstring`,
-
-        category: "bottomwear",
-        price: "300 EGP",
-      }
+   
   ];
 
 
